9eb3b77324fee84fb9409412ba017cf78c9f7b70
Sourabh Singh <singhsourabh90@gmail.com>
2015-08-21 01:01:31 +0530

    made some changes

--- english.py
+++ english.py
@@ -12,16 +12,43 @@ def getSoup(url):
 
 def getMeaning(word):
 	soup = getSoup('http://www.merriam-webster.com/dictionary/'+word)
-	meaning=soup.find_all('div',class_='content default')
-	print(meaning)
-	getAntonymsAndSynomyms(word,soup)
-	getSynonymsDiscussion(word,soup)
+	all_definitions = soup.find_all('span',class_='ssens')
+	meaning = []
+	for p in all_definitions:
+		meaning.append(p.text)
+	return meaning
+	
+def getAntonyms(word):
+	soup = getSoup('http://www.thesaurus.com/browse/'+word)
+	all_antonyms = soup.find_all('div',class_='list-holder')
+	if len(all_antonyms) > 0:
+		all_antonyms = all_antonyms[0].find_all('span',class_='text')
+	meaning = []
+	for p in all_antonyms:
+		meaning.append(p.text)
+	return meaning
 
-def getAntonymsAndSynomyms(word,soup):
-	return soup.find_all('div',class_='content default')
+def getSynomyms(word):
+	soup = getSoup('http://www.thesaurus.com/browse/'+word)
+	all_synomyms = soup.find_all('div',class_='relevancy-list')[0].find_all('span',class_='text')
+	synomyms = []
+	for p in all_synomyms:
+		synomyms.append(p.text)
+	return synomyms
 
-def getSynonymsDiscussion(word,soup):
+def getSynonymsDiscussion(word):
+	soup = getSoup('http://www.merriam-webster.com/dictionary/'+word)
 	return soup.find_all('div',class_='synonyms-discussion')
+
+def getEtymology(word):
+	url = 'http://www.etymonline.com/index.php?term='+word+'&allowed_in_frame=0'
+	soup=getSoup(url)
+	return soup.find_all('dd',class_='highlight')[0].text
+
+def getMnenomonic(word):
+	url = 'http://mnemonicdictionary.com/word/'+word        
+	soup = getSoup(url)
+	return soup.find_all('div',class_='span9')[0].text
 
 def getExamples(word):
 	url='http://corpus.vocabulary.com/api/1.0/examples.json?maxResult=24&query='+word
@@ -29,12 +56,7 @@ def getExamples(word):
 	example = []
 	for s in data['result']['sentences']:
 		example.append(s['sentence'])
-	print(example)
-
-def getEtymology(word):
-	url = 'http://www.etymonline.com/index.php?term='+word+'&allowed_in_frame=0'
-	soup=getSoup(url)
-	return soup.find_all('dd',class_='highlight')[0].text
+	return example
 
 def getImage(word):
 	url = 'http://wordpandit.com/2012/'+word+'/'
@@ -43,20 +65,20 @@ def getImage(word):
 	imgUrl = content[0]['src']
 	image = urllib.urlretrieve(imgUrl, os.path.basename('/images/'+imgUrl))
 
-def getMnenomonic(word):
-	url = 'http://mnemonicdictionary.com/word/'+word        
-	soup = getSoup(url)
-	return soup.find_all('div',class_='span9')[0].text
-
 def getWordDetails(word,title,desc):
 	data = {}
 	data['word'] = word
 	data['theme-title'] = title
 	data['theme-desc'] = desc
+	data['meaning'] = getMeaning(word)
+	data['Antonyms'] = getAntonyms(word)
+	data['Synomyms'] = getSynomyms(word)
 	data['etymology'] = getEtymology(word)
 	data['mnemonic'] = getMnenomonic(word)
-	print(getExamples(word)) 
-	#print(data)
+	data['examples'] = getExamples(word)
+	for k in data:
+		print(k)
+		print(data[k]) 
 
 def allWordsByGroup(url):
 	soup = getSoup(url)
@@ -68,6 +90,7 @@ def allWordsByGroup(url):
 		wordData = getWordDetails(word,title,desc)
 
 def main():
+	#maximum possible is 95
 	for i in range(1,2):
 		url = 'https://www.greedge.com/grewordlist/words/'+str(i)
 		allWordsByGroup(url)
